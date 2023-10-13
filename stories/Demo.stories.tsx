06b5dbfa027fd3d0187cbba197d95c2550a4073a
RyoSogawa <koashimitekara.1122@gmail.com>
2023-10-13 22:46:32 +0900

    chore:add Demo story

---
+++ stories/Demo.stories.tsx
@@ -0,0 +1,12 @@
+import Demo from './Demo';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Demo> = {
+  component: Demo,
+};
+
+export default meta;
+type Story = StoryObj<typeof Demo>;
+
+export const Primary: Story = {};
---
+++ stories/Demo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Demo: React.FC = () => (
+  <main>
+    <header>Header</header>
+    <div>
+      <textarea placeholder="focus here" />
+    </div>
+    <footer>Footer</footer>
+  </main>
+);
+
+export default Demo;
